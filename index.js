5721439d0d99ca84ffb382b74f2d0c0c7844a9bc
Juan Ignacio Donoso <jidonoso@gmail.com>
2017-12-06 17:51:04 -0500

    feat(): adds logs when changing values

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Config = require('cordova-config');
 const path = require('path');
 
+const { log } = require('./lib/utils')
+
 const SOURCE_PATH = process.env['BITRISE_SOURCE_DIR'];
 const CONFIG_FILE_PATH = process.env['config_path'] || 'config.xml'
 
@@ -17,26 +19,38 @@ const config = new Config(path.join(SOURCE_PATH, CONFIG_FILE_PATH));
 
 if(NAME) {
   config.setName(NAME);
+
+  log('name', NAME);
 }
 
 if(ID) {
   config.setID(ID);
+
+  log('id', ID);
 }
 
 if (ANDROID_VERSION_CODE) {
   config.setAndroidVersionCode(ANDROID_VERSION_CODE);
+
+  log('android-versionCode', ANDROID_VERSION_CODE);
 }
 
 if (ANDROID_PACKAGE_NAME) {
   config.setAndroidPackageName(ANDROID_PACKAGE_NAME);
+
+  log('android-packageName', ANDROID_PACKAGE_NAME);
 }
 
 if (IOS_BUNDLE_VERSION) {
   config.setIOSBundleVersion(IOS_BUNDLE_VERSION);
+
+  log('ios-CFBundleVersion', IOS_BUNDLE_VERSION);
 }
 
 if (IOS_BUNDLE_IDENTIFIER) {
   config.setIOSBundleIdentifier(IOS_BUNDLE_IDENTIFIER);
+
+  log('ios-CFBundleIdentifier', IOS_BUNDLE_IDENTIFIER);
 }
 
 // Write the config file
